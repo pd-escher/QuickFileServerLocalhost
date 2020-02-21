7adb19dd0113ca8e4cfd6d11ff234e857eedd668
pd-escher <dhoolia.pranav@gmail.com>
2020-02-21 22:18:37 +0530

    Testing Heroku

--- server.js
+++ server.js
@@ -2,15 +2,15 @@ const fs = require('fs')
 const path = require('path')
 
 var express = require('express');
-var app = express();
+const app = express();
 
 // deployed to heroku
 
-function folderpath() {
-    return process.argv.length > 2 ? process.argv[2] : './APK/hello'
-}
+// function folderpath() {
+//     return process.argv.length > 2 ? process.argv[2] : './APK/hello'
+// }
 
-const child_process = require('child_process');
+// const child_process = require('child_process');
 
 // UI
 app.get('/', function (req, res) {
@@ -18,23 +18,23 @@ app.get('/', function (req, res) {
 });
 
 // APKs
-app.get("/apk/", (req, res) => {
+// app.get("/apk/", (req, res) => {
 
-    const APK = './APK'
-    const folder = 'hello'
+//     const APK = './APK'
+//     const folder = 'hello'
 
-    // we want to use a sync exec to prevent returning response
-    // before the end of the compression process
-    child_process.execSync(`zip -r archive *`, {
-        cwd: folderpath()
-    });
+//     // we want to use a sync exec to prevent returning response
+//     // before the end of the compression process
+//     child_process.execSync(`zip -r archive *`, {
+//         cwd: folderpath()
+//     });
 
-    // zip archive of your folder is ready to download
-    res.download(folderpath() + `/archive.zip`);
-});
+//     // zip archive of your folder is ready to download
+//     res.download(folderpath() + `/archive.zip`);
+// });
 
 // Listen
-const port = process.argv.length > 3 ? process.argv[3] : 8080
-app.listen(port);
+
+app.listen(process.env.PORT || 8080);
 console.log('Listening on localhost:' + port);
 
